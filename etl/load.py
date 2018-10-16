01a6efc6b7d7162cf3e05a64af74b29fa1c093ca
Andre Pereira <andrespp@gmail.com>
2018-10-16 17:21:25 -0300

    Extract script now writes 'programas's' parquet file

--- etl/load.py
+++ etl/load.py
@@ -6,18 +6,20 @@ import pandas as pd
 ## Definitions
 dataset_home = "./dataset"
 datasrc_home = "./datasrc"
-md5sums_file = "./datasrc/MD5SUMS"
 
-# Dataset's datasrc
-# Fields: dataset, srcfile
-table = [
+datasrc_table = [
  ["programas", "br-capes-colsucup-prog-2013a2016-2017-12-02_2013.csv.gz"],
  ["programas", "br-capes-colsucup-prog-2013a2016-2017-12-02_2014.csv.gz"],
  ["programas", "br-capes-colsucup-prog-2013a2016-2017-12-02_2015.csv.gz"],
  ["programas", "br-capes-colsucup-prog-2013a2016-2017-12-02_2016.csv.gz"],
  ["programas", "br-capes-colsucup-prog-2017-2018-08-01.csv.gz"]]
+df = pd.DataFrame(datasrc_table, columns='dataset srcfile'.split())
 
-df = pd.DataFrame(table, columns='dataset srcfile'.split())
+dataset_table = [
+ ["docentes", "br-capes-docentes.parquet"],
+ ["discentes", "br-capes-discentes.parquet"],
+ ["programas", "br-capes-programas.parquet"]]
+ds_df = pd.DataFrame(dataset_table, columns='dataset filename'.split())
 
 ## Functions
 def read_datasrc(filename, sep=';', encoding='iso-8859-1', index_col=17,
@@ -46,20 +48,9 @@ def read_datasrc(filename, sep=';', encoding='iso-8859-1', index_col=17,
         print('WARN. Could open "{}".'.format(filename))
         return(-1)
 
-## Read MD5SUMS
-try:
-    with open(md5sums_file, 'r') as f:
-        md5sums = pd.read_csv(md5sums_file, sep=' ', header=None,
-                                names=['md5sum', 'lfilename'])
+## Main
 
-except FileNotFoundError:
-    print('Error. Could open "{}". Run extract step first'.\
-        format(md5sums_file))
-    exit(-1)
-
-## Build .parquet from .csv.gz
-
-# 'Programas' dataset
+# 'Programas's dataframe
 
 programas = read_datasrc(datasrc_home + '/' + \
                            df[df['dataset'] == 'programas'].loc[0]['srcfile'],\
@@ -72,29 +63,7 @@ for i in df[df['dataset'] == 'programas']['srcfile']:
     programas = programas.append(read_datasrc(datasrc_home + '/' + i),\
                                     ignore_index=True)
 
-print(programas.index)
-print(programas.columns)
-
-exit(0)
-
-df = list()
-for i in range(len(csv_files)):
-    print(basedir + csv_files[i])
-    df.append(pd.read_csv(basedir + csv_files[i], index_col=17, sep=';',encoding='iso-8859-1'))
-    print("{} read.".format(csv_files[i]))
-
-discentes = pd.concat(df)
-
-
-# In[4]:
-
-
-discentes.head()
-
-
-# In[5]:
-
-
-table = pa.Table.from_pandas(discentes) #, timestamps_to_ms=True)
-pq.write_table(table, './dataset/br-capes-colsucup-discentes.parquet', compression='GZIP')
-
+# 'Programa's .parquet file
+pq_fname = dataset_home + '/' + \
+              ds_df[ds_df['dataset'] == 'programas'].iloc[0]['filename']
+pq.write_table(pa.Table.from_pandas(programas), pq_fname, compression='GZIP')
